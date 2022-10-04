a2e147014e40ebb0e3022765d3db3bb29a4947c9
dya-code <entrydia7691@gmail.com>
2022-10-04 22:00:26 +0900

    api/timetable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
+const laftel = require('laftel.js')
 const cors = require('cors')
 const port = process.env.PORT || 3000
 
@@ -8,8 +9,20 @@ app.use(cors())
 
 app.get('/api/timetable', async (_, res) => {
 
+  let arr = []
+  let response = []
+  
   const day = new Date().getDay()
-  await axios.get('https://anissia.net/api/anime/schedule/0').then( resp => res.send(resp.data) )
+  await axios.get('https://anissia.net/api/anime/schedule/' + day).then( resp => response = resp.data )
+
+  for (let i = 0; i < response.length; i++) {
+    await laftel.search(response[i].subject).then(result => {
+      arr.push(result.results[0].img)
+    })
+  }
+
+  res.send({ response, arr })
+
 })
 
-app.listen(port, () => { console.log(`http://localhost:${port}/`) })
+app.listen(port, () => { console.log(`http://localhost:${port}/\n========================================\n========================================`) })
