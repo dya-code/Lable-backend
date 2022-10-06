4979a4c63c7c15974f92d1d57670df7405475198
dya-code <entrydia7691@gmail.com>
2022-10-06 18:19:08 +0900

    remade

--- src/app.js
+++ src/app.js
@@ -11,17 +11,21 @@ app.get('/api/timetable', async (_, res) => {
 
   let arr = []
   let response = []
-  
-  const day = new Date().getDay()
-  await axios.get('https://anissia.net/api/anime/schedule/' + day).then( resp => response = resp.data )
+
+  await axios.get('https://anissia.net/api/anime/schedule/' + new Date().getDay()).then( resp => response = resp.data )
 
   for (let i = 0; i < response.length; i++) {
+
+    let title = response[i].subject
+    let img = ''
     await laftel.search(response[i].subject).then(result => {
-      arr.push(result.results[0].img)
+      img = result.results[0].img
     })
+
+    arr.push({ title, img })
   }
   
-  res.send({ response, arr })
+  res.send(arr)
 })
 
 app.listen(port, () => { console.log(`http://localhost:${port}/\n========================================\n========================================`) })
